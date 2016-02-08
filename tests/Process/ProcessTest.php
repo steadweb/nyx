35651826b176f8983087932ed87f04b85c160ad1
Luke Steadman <luke.steadman@jadu.co.uk>
2016-02-08 15:49:37 +0000

    [FEATURE] initial tests added

---
+++ tests/Process/ProcessTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Nyx\Tests\Process;
+
+use Nyx\Command;
+use Nyx\Process;
+use Nyx\Tests\Pool\NullOutput;
+
+class ProcessTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Process
+     */
+    protected $process;
+
+    public function setUp()
+    {
+        $this->process = new Process(new Command('echo foo'));
+        $this->process->setOutput(new NullOutput());
+    }
+
+    public function testSetCommand()
+    {
+        $this->assertEquals('echo foo', $this->process->getCommand()->getCmd());
+
+        $command = new Command('echo bar');
+        $this->process->setCommand($command);
+
+        $this->assertEquals('echo bar', $this->process->getCommand()->getCmd());
+    }
+
+    public function testOpenProcess()
+    {
+        $this->process->open();
+
+        $this->assertTrue(true, $this->process->isRunning());
+        $this->assertEquals(1, $this->process->status());
+
+        $this->process->close();
+    }
+
+    public function testStoppedProcess()
+    {
+        $this->process->open();
+
+        $this->assertTrue(true, $this->process->isRunning());
+        $this->assertEquals(1, $this->process->status());
+
+        $this->process->close();
+
+        $this->assertFalse(false, $this->process->isRunning());
+        $this->assertEquals(2, $this->process->status());
+
+    }
+}
