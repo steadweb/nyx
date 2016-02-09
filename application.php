7ec14947def221582715f11950467cd9e6a66313
Luke Steadman <luke.steadman@jadu.co.uk>
2016-02-09 10:05:32 +0000

    [FEATURE] added symfony/console to deps

---
+++ application.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+declare(ticks = 1);
+
+require __DIR__.'/vendor/autoload.php';
+
+use Nyx\NyxCommand;
+use Symfony\Component\Console\Application;
+
+$application = new Application(\Nyx\Manager::NAME, \Nyx\Manager::VERSION);
+$application->add(new NyxCommand());
+$application->run();
