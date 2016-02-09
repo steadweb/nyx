f1a74acb32c94828aa1b16801d30bc6c6debd568
Luke Steadman <luke.steadman@jadu.co.uk>
2016-02-09 10:28:32 +0000

    [FEATURE] added symfony/console to aid command line functionality

---
+++ src/Nyx/NyxCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nyx;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NyxCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('nyx:run')
+            ->setDescription('Nyx run command which boots your workers.')
+            ->addArgument(
+                'config',
+                InputArgument::REQUIRED,
+                'Path to your nyx.json config'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = Manager::factory($input->getArgument('config'));
+        $manager->run();
+    }
+}
