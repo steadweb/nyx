38232c8d37df7b3e97e1159b0019456adc5f7e96
Luke Steadman <luke.steadman@jadu.co.uk>
2016-02-09 12:16:27 +0000

    [FEATURE] namespaced commands to Nyx\Command and added TestCommand

--- application.php
+++ application.php
@@ -5,9 +5,11 @@ declare(ticks = 1);
 
 require __DIR__.'/vendor/autoload.php';
 
-use Nyx\NyxCommand;
+use Nyx\Commands\NyxCommand;
+use Nyx\Commands\TestCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application(\Nyx\Manager::NAME, \Nyx\Manager::VERSION);
 $application->add(new NyxCommand());
+$application->add(new TestCommand());
 $application->run();
--- src/Nyx/Commands/NyxCommand.php
+++ src/Nyx/Commands/NyxCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Nyx;
+namespace Nyx\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
